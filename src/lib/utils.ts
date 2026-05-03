2a73c68c896b6dfa95a9c20459900d2ec506769b
bongnteh-romarick-ndzelen <ndzelenromarick@gmail.com>
2026-05-04 00:54:40 +0100

    adding this love and unlove on a blog

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(dateString: string) {
+export function formatDate(dateString: string | Date) {
   const date = new Date(dateString);
   if (isNaN(date.getTime())) {
     return "Date not available";
